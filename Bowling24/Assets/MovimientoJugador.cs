31b9e051124d5639d1bb5dd96cc7e451396287bc
Nu36 <nurirt36@uma.es>
2024-05-06 19:28:24 +0200

    MovimientoJugador script

---
+++ Bowling24/Assets/MovimientoJugador.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pepe
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
