defd4959e5dc29f2dab9d5e34934199c1e15f900
Ignacio Martínez Gallardo <ignamarga@gmail.com>
2024-05-20 20:41:39 +0200

    Añadido recorrido Navmesh

---
+++ Bowling24/Assets/Scripts/ComportamientoJugador.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ComportamientoJugador : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject[] pathPositions;
+    public int actualObjective;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (agent == null)
+        {
+            agent = GetComponent<NavMeshAgent>();
+            agent.SetDestination(pathPositions[0].transform.position);   // The first path position the agent has to go is the 0
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When the agent reaches the position
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            actualObjective++;
+
+            // Sets the next position of the path
+            if (actualObjective >= pathPositions.Length)
+            {
+                actualObjective = 0;
+            }
+            agent.destination = pathPositions[actualObjective].transform.position;
+        }
+    }
+}
